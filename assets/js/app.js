e07128e1ac3b1bcd770bddd37de03459ecab3a80
Sahil Kunder <118273597+sahilkunder@users.noreply.github.com>
2022-12-12 13:44:59 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,17 @@ const featureSection = document.getElementById('inn')
 const progressBars = document.querySelectorAll('.w3-progressbar')
 
 function showProgress(){
- progressBars.forEach(progressBar=> {
+ progressBars.forEach(progressBar => {
   const value = progressBar.dataset.progress;
   progressBar.style.opacity = 1;
   progressBar.style.width = `${value}%`;
+ });
+}
+
+function hideProgress(){
+ progressBars.forEach(p => {
+  p.style.opacity = 0;
+  p.style.width = 0;
  });
 }
 
