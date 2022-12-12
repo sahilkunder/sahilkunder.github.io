b48c36cf072c1ebe85337503ae00eb65d2106aa9
Sahil Kunder <118273597+sahilkunder@users.noreply.github.com>
2022-12-12 13:14:00 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,22 @@ const featureSection = document.getElementById('.w3-progress-container')
 
 const progressBars = document.querySelectorAll('.w3-progressbar')
 
+function showProgress(){
+ progressBars.forEach(progressBar=> {
+  const value = progressBar.dataset.progress;
+  progressBar.style.opacity = 1;
+  progressBar.style.width = `${value}%`;
+ });
+}
 
 window.addEventListener('scroll',() => {
  const sectionPos = featureSection.getBoundingClientRect().top; 
  const screenPos = window.innerHeight;
-  
-    if(sectionPos < screenPos){
-      showProgress();
-    }else{
-      hideProgress();
-    }
-  
+ 
+ if(sectionPos < screenPos){
+  showProgress();
+ }else{
+  hideProgress();
+ }
+ 
 });
